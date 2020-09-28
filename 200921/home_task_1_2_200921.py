411633cfe174dc509873df25ff30ae2f26e76e45
Slick <holygalaxy444@gmail.com>
2020-09-28 09:39:25 +0500

    2009

--- 200921/home_task_1_2_200921.py
+++ 200921/home_task_1_2_200921.py
@@ -1,26 +1,26 @@
 #1
-#row_record = '217.168.17.5 - - [ 17/May/2015:08:05:12 +0000] "GET /downloads/product_2 HTTP/1.1" 200 3316 "-" "-"'
+#row_record = '217.168.17.5 - - [17/May/2015:08:05:12 +0000] "GET /downloads/product_2 HTTP/1.1" 200 3316 "-" "-"'
 #correct_request_IP_address = '217.168.17.5'
 #correct_request_datetime = '17/May/2015:08:05:12'
 
-#request_IP_address = row_record.split() [0]
-#request_datetime = row_record.split() [4]
+#request_IP_address = row_record.split()[0]
+#request_datetime = row_record.split()[4]
 #print(request_IP_address, ", ", request_datetime)
 
 #assert request_IP_address == correct_request_IP_address
 #assert request_datetime == correct_request_datetime
 
 #2*
-#row_record = '217.168.17.5 - - [ 17/May/2015:08:05:12 +0000] "GET /downloads/product_2 HTTP/1.1" 200 3316 "-" "-"'
+#row_record = '217.168.17.5 - - [17/May/2015:08:05:12 +0000] "GET /downloads/product_2 HTTP/1.1" 200 3316 "-" "-"'
 #correct_request_IP_address = '217.168.17.5'
 #correct_request_datetime = '17/May/2015:08:05:12'
 #correct_request_method = 'GET'
 #correct_requested_resource = '/downloads/product_2'
 
-#request_IP_address = row_record.split() [0]
-#request_datetime = row_record.split() [4]
-#request_method = row_record.split() [6]
-#requested_resource = row_record.split() [7]
+#request_IP_address = row_record.split()[0]
+#request_datetime = row_record.split()[4]
+#request_method = row_record.split()[6]
+#requested_resource = row_record.split()[7]
 #print(request_IP_address, ", ", request_datetime, ", ", request_method, ", ", requested_resource)
 
 #assert request_IP_address == correct_request_IP_address
